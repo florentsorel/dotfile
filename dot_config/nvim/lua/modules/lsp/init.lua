5932a12b55ef0273638678ae7a023a4fb0f0cc18
Rtransat <florent.sorel@gmail.com>
2021-11-24 00:04:17 +0100

    refactor les capabilities

--- dot_config/nvim/lua/modules/lsp/init.lua
+++ dot_config/nvim/lua/modules/lsp/init.lua
@@ -37,9 +37,8 @@ local on_attach = function(_, bufnr)
 end
 
 -- nvim-cmp supports additional completion capabilities
-local capabilities = vim.lsp.protocol.make_client_capabilities()
---capabilities.textDocument.completion.completionItem.snippetSupport = true
---capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 -- Enable the following language servers
 local servers = { 'intelephense', 'cssls', 'html', 'tsserver', 'gopls', 'rust_analyzer', }
