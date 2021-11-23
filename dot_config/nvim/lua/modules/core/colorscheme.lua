4d813a3d55b512305bb2752b9b4405f18f33aad6
Rtransat <florent.sorel@gmail.com>
2021-11-23 09:13:53 +0100

    applique le theme dracula pour alacritty et neovim

--- dot_config/nvim/lua/modules/core/colorscheme.lua
+++ dot_config/nvim/lua/modules/core/colorscheme.lua
@@ -1,28 +1,8 @@
 -- Colorscheme
 
--- https://github.com/marko-cerovac/material.nvim
-vim.g.material_style = 'palenight'
+-- https://github.com/Mofiqul/dracula.nvim
+--
 
-require('material').setup({
-    contrast = false,
-    italics = {
-        comments = true,
-        functions = false,
-    },
-    contrast_windows = {
-        'terminal',
-        'packer',
-        'qf',
-    },
-    text_contrast = {
-        lighter = false,
-        darker = false,
-    },
-    disable = {
-        background = false,
-    },
-})
-
-vim.cmd([[colorscheme material]])
+vim.cmd([[colorscheme dracula]])
 
 vim.api.nvim_exec([[au BufRead,BufNewFile ~/.config/i3/config set filetype=i3config]], false)
