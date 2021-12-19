5017359639dd1346e7d660ff5dc454aaf456c13c
Rtransat <florent.sorel@gmail.com>
2021-12-19 14:48:44 +0100

    feat(nvim): ajoute des dossiers à ignorer pour telescope

---
+++ dot_config/nvim/lua/modules/plugins/telescope.lua
@@ -0,0 +1,16 @@
+-- Telescope
+
+require('telescope').setup{
+    defaults = {
+        file_ignore_patterns = {
+            "./node_modules/*",
+            "node_modules",
+            "^node_modules/*",
+            "node_modules/*",
+            ".git",
+            "./git",
+            "vendor",
+            "./vendor",
+        },
+    }
+}
